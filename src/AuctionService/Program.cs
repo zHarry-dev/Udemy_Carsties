f603e1847a4766654400a285e8382d60c58f0a03
Nguyễn Phương <nguyenphuong@MacBook-Pro-cua-Phuong.local>
2024-01-18 15:37:44 +0700

    commit macbook

--- src/AuctionService/Program.cs
+++ src/AuctionService/Program.cs
@@ -24,6 +24,11 @@ builder.Services.AddMassTransit(x =>
     x.SetEndpointNameFormatter(new KebabCaseEndpointNameFormatter("auction", false));
     x.UsingRabbitMq((context, cfg) =>
     {
+        cfg.Host(builder.Configuration["RabbitMq:Host"], "/", host =>
+        {
+            host.Username(builder.Configuration.GetValue("RabbitMq:Username", "guest"));
+            host.Username(builder.Configuration.GetValue("RabbitMq:Password", "guest"));
+        });
         cfg.ConfigureEndpoints(context);
     });
 });
