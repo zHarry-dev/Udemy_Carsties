1dae26ae990b07d05568411312eb56720aea4f56
Nguyễn Phương <nguyenphuong@MacBook-Pro-cua-Phuong.local>
2024-01-25 13:50:32 +0700

    commit at Macbook

--- frontend/web-app/app/actions/auctionAction.ts
+++ frontend/web-app/app/actions/auctionAction.ts
@@ -2,8 +2,8 @@
 
 import { Auction, PageResult } from "@/types";
 
-export async function getData(pageNumber : number = 1, pageSize: number = 4): Promise<PageResult<Auction>> {
-    const res = await fetch(`http://localhost:6001/search?pageSize=${pageSize}&pageNumber=${pageNumber}`);
+export async function getData(query: string): Promise<PageResult<Auction>> {
+    const res = await fetch(`http://localhost:6001/search?${query}`);
 
     if (!res.ok) {
         throw new Error('Fail to get data');
--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -3,28 +3,38 @@
 import React, { useEffect, useState } from 'react'
 import AuctionCard from './AuctionCard';
 import AppPagination from '../components/AppPagination';
-import { Auction } from '@/types';
+import { Auction, PageResult } from '@/types';
 import { getData } from '../actions/auctionAction';
 import Filters from './Filters';
+import { useParamsStore } from '@/hooks/useParamsStore';
+import { shallow } from 'zustand/shallow';
+import qs from 'query-string';
 
 export default function Listings() {
-    const [auctions, setAuctions] = useState<Auction[]>([])
-    const [pageCount, setPageCount] = useState(0)
-    const [pageNumber, setPageNumber] = useState(1)
-    const [pageSize, setPageSize] = useState(4)
+    const [data, setData] = useState<PageResult<Auction>>();
+    const params = useParamsStore(state => ({
+        pageNumber: state.pageNumber,
+        pageSize: state.pageSize,
+        searchTerm: state.searchTerm
+    }), shallow);
+    const setParams = useParamsStore(state => state.setParams)
+    const url = qs.stringifyUrl({ url: '', query: params })
+
+    function setPageNumber(pageNumber: number) {
+        setParams({pageNumber})
+    }
 
     useEffect(() => {
-        getData(pageNumber, pageSize).then(data => {
-            setAuctions(data.results);
-            setPageCount(data.pageCount);
+        getData(url).then(data => {
+            setData(data);
         })
-    }, [pageNumber, pageSize])
+    }, [url])
 
-    if (auctions.length === 0) return <h3>Loading...</h3>
+    if (!data) return <h3>Loading...</h3>
 
     return (
         <>
-            <Filters pageSize={pageSize} setPageSize={setPageSize} />
+            <Filters pageSize={params.pageSize} setPageSize={setPageSize} />
             <div className='grid grid-cols-4 gap-6'>
                 {auctions.map(auction => (
                     <AuctionCard auction={auction} key={auction.id} />
