c493319fb492a6de108a3fa07a4135d6dec64670
Phuong Nguyen <nnphuongou2014@gmail.com>
2024-01-29 14:01:39 +0700

    End of Section 10

--- frontend/web-app/app/actions/auctionAction.ts
+++ frontend/web-app/app/actions/auctionAction.ts
@@ -1,35 +1,36 @@
 'use server'
 
 import { Auction, PageResult } from "@/types";
-import { getTokenWorkaround } from "./authActions";
+import { fetchWrapper } from "@/lib/fetchWrapper";
+import { FieldValues } from "react-hook-form";
+import { revalidatePath } from "next/cache";
 
 export async function getData(query: string): Promise<PageResult<Auction>> {
-    const res = await fetch(`http://localhost:6001/search${query}`);
-
-    if (!res.ok) {
-        throw new Error('Fail to get data');
-    }
-
-    return res.json();
+    return await fetchWrapper.get(`search${query}`);
 }
 
-export async function UpdateAuctionTest() {
+export async function updateAuctionTest() {
     const data = {
         mileage: Math.floor(Math.random() * 100000) + 1
     }
 
-    const token = await getTokenWorkaround();
+    return await fetchWrapper.put('auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c', data);
+}
 
-    const res = await fetch('http://localhost:6001/auctions/afbee524-5972-4075-8800-7d1f9d7b0a0c', {
-        method: 'PUT',
-        headers: {
-            'Content-type': 'application/json',
-            'Authorization': 'Bearer ' + token?.accessToken
-        },
-        body: JSON.stringify(data)
-    })
+export async function createAuction(data: FieldValues) {
+    return await fetchWrapper.post('auctions', data);
+}
 
-    if (!res.ok) return { status: res.status, message: res.statusText }
+export async function updateAuction(data: FieldValues, id: string) {
+    return await fetchWrapper.put(`auctions/${id}`, data);
+}
 
-    return res.statusText;
+export async function deleteAuction(id: string) {
+    return await fetchWrapper.del(`auctions/${id}`);
+}
+
+export async function getDetailedViewData(id: string): Promise<Auction> {
+    const res = await fetchWrapper.get(`auctions/${id}`);
+    revalidatePath(`auctions/${id}`);
+    return res;
 }
--- frontend/web-app/app/nav/UserAction.tsx
+++ frontend/web-app/app/nav/UserAction.tsx
@@ -1,23 +1,40 @@
 'use client'
 
+import { useParamsStore } from '@/hooks/useParamsStore'
 import { Dropdown } from 'flowbite-react'
 import { User } from 'next-auth'
 import { signOut } from 'next-auth/react'
 import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import { useRouter } from 'next/navigation'
 import React from 'react'
-import { AiFillCar, AiFillTrophy, AiOutlineLogin, AiOutlineLogout } from 'react-icons/ai'
+import { AiFillCar, AiFillTrophy, AiOutlineLogout } from 'react-icons/ai'
 import { HiCog, HiUser } from 'react-icons/hi'
 
 type Props = {
-  user: Partial<User>
+  user: User
 }
 
 export default function UserAction({ user }: Props) {
+  const router = useRouter();
+  const pathname = usePathname();
+  const setParams = useParamsStore(state => state.setParams);
+
+  function setWinner() {
+    setParams({ winner: user.username, seller: undefined })
+    if (pathname !== '/') router.push('/');
+  }
+
+  function setSeller() {
+    setParams({ seller: user.username, winner: undefined })
+    if (pathname !== '/') router.push('/');
+  }
+
   return (
     <Dropdown label={`Welcome ${user.name}`} inline>
-      <Dropdown.Item icon={HiUser}>My Auctions</Dropdown.Item>
-      <Dropdown.Item icon={AiFillTrophy}>Auction won</Dropdown.Item>
-      <Dropdown.Item icon={AiFillCar}>Sell my car</Dropdown.Item>
+      <Dropdown.Item icon={HiUser}><Link href='/' onClick={setSeller}>My Auctions</Link></Dropdown.Item>
+      <Dropdown.Item icon={AiFillTrophy}><Link href='/' onClick={setWinner}>Auction won</Link></Dropdown.Item>
+      <Dropdown.Item icon={AiFillCar}><Link href='/auctions/create'>Sell my car</Link></Dropdown.Item >
       <Dropdown.Item icon={HiCog}>
         <Link href='/session'>
           Session (dev only)
@@ -25,6 +42,6 @@ export default function UserAction({ user }: Props) {
       </Dropdown.Item>
       <Dropdown.Divider />
       <Dropdown.Item icon={AiOutlineLogout} onClick={() => signOut({ callbackUrl: '/' })}>Sign out</Dropdown.Item>
-    </Dropdown>
+    </Dropdown >
   )
 }
