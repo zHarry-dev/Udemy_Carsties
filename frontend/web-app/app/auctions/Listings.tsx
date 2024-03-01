ee2d94f9a19d5afee5a1fb62d9e27ae31b024071
Macbook Pro of Phuong <nnphuongou2014@gmail.com>
2024-03-01 18:23:00 +0700

    Commit Macbook

--- frontend/web-app/app/auctions/Listings.tsx
+++ frontend/web-app/app/auctions/Listings.tsx
@@ -3,16 +3,16 @@
 import React, { useEffect, useState } from 'react'
 import AuctionCard from './AuctionCard';
 import AppPagination from '../components/AppPagination';
-import { Auction, PageResult } from '@/types';
 import { getData } from '../actions/auctionAction';
 import Filters from './Filters';
 import qs from 'query-string';
 import { useParamsStore } from '@/hooks/useParamsStore';
 import { shallow } from 'zustand/shallow';
 import EmptyFilter from '../components/EmptyFilter';
+import { useAuctionStore } from '@/hooks/useAuctionStore';
 
 export default function Listings() {
-    const [data, setData] = useState<PageResult<Auction>>();
+    const [loading, setLoading] = useState(true);
     const params = useParamsStore(state => ({
         pageNumber: state.pageNumber,
         pageSize: state.pageSize,
@@ -22,6 +22,12 @@ export default function Listings() {
         seller: state.seller,
         winner: state.winner
     }), shallow);
+    const data = useAuctionStore(state => ({
+        auctions: state.auctions,
+        totalCount: state.totalCount,
+        pageCount: state.pageCount
+    }), shallow);
+    const setData = useAuctionStore((state) => state.setData);
     const setParams = useParamsStore(state => state.setParams)
     const url = qs.stringifyUrl({ url: '', query: params })
 
@@ -31,11 +37,14 @@ export default function Listings() {
 
     useEffect(() => {
         getData(url).then(data => {
+            console.log(url);
+            console.log(data);
             setData(data);
+            setLoading(false);
         })
     }, [url])
 
-    if (!data) return <h3>Loading...</h3>
+    if (loading) return <h3>Loading...</h3>
 
     return (
         <>
@@ -44,8 +53,8 @@ export default function Listings() {
                 <EmptyFilter showReset />
             ) : (
                 <>
-                    <div className='grid grid-cols-4 gap-6'> 
-                        {data.results.map(auction => (
+                    <div className='grid grid-cols-4 gap-6'>
+                        {data.auctions.map(auction => (
                             <AuctionCard auction={auction} key={auction.id} />
                         ))}
                     </div>
---
+++ frontend/web-app/hooks/useAuctionStore.ts
@@ -0,0 +1,38 @@
+import { Auction, PageResult } from "@/types"
+import { createWithEqualityFn } from "zustand/traditional"
+
+type State = {
+    auctions: Auction[]
+    totalCount: number
+    pageCount: number
+}
+
+type Actions = {
+    setData: (data: PageResult<Auction>) => void
+    setCurrentPrice: (auctionId: string, amount: number) => void
+}
+
+const initialState: State = {
+    auctions: [],
+    pageCount: 0,
+    totalCount: 0
+}
+
+export const useAuctionStore = createWithEqualityFn<State & Actions>((set) => ({
+    ...initialState,
+
+    setData: (data: PageResult<Auction>) => {
+        set(() => ({
+            auctions: data.results,
+            pageCount: data.pageCount,
+            totalCount: data.totalCount
+        }))
+    },
+
+    setCurrentPrice: (auctionId: string, amount: number) => {
+        set((state) => ({
+            auctions: state.auctions.map((auction) => auction.id == auctionId
+                ? { ...auction, currentHighBid: amount } : auction)
+        }))
+    }
+}))
