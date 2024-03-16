c48938b3a283a4d62f84de77087ad242ed4262d2
Macbook Pro of Phuong <nnphuongou2014@gmail.com>
2024-03-16 11:32:02 +0700

    Finished Main Course

--- src/IdentityService/Models/ApplicationUser.cs
+++ src/IdentityService/Models/ApplicationUser.cs
@@ -9,4 +9,5 @@ namespace IdentityService.Models;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
+    
 }
